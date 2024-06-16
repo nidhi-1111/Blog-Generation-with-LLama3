47a36ea0e573239c1268c1bb18f3bef5eee80583
nidhi-1111 <nidhibodar17@gmail.com>
2024-06-15 21:54:20 -0400

    connected to aws bedrock

--- app.py
+++ app.py
@@ -1,66 +1,54 @@
-import streamlit as st
-from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
-from langchain_groq import ChatGroq 
-from langchain_core.prompts import ChatPromptTemplate
+import boto3
+import botocore.config
+import json
+from datetime import datetime 
 
-from dotenv import load_dotenv
-load_dotenv()
-import os
+def blog_gen_bedrock(blogtopic:str)->str:
+    prompt=f"""<s>[INST]Human: Write a 200 words blog on the topic {blogtopic}
+    Assistant:[/INST]
+    """
+    body ={
+        "prompt": prompt,
+        "max_gen_len":512,
+        "temperature":0.3,
+        "top_p": 0.9
+    }
+    try:
+        bedrock = boto3.client("bedrock-runtime", region="us-east-1",
+                               config=botocore.config.Config(read_timeout=300,retries={'max_attempts':3}))
+        response = bedrock.invoke_model(body=json.dumps(body), modelId='meta.llama2-13b-chat-v1')
+        response_content = response.get('body').read()
+        response_data = json.loads(response_content)
+        blog_details = response_data['generation']
+        return blog_details
+    except Exception as e:
+        print(f"Error while generating the blog : {e}")
+        return ""
+    
 
-## Load the Groq API key
-groq_api_key = os.getenv('GROQ_API_KEY')
-# Funciton to get respone from LLAma 2 model
+def save_blog_s3(s3_key,s3_bucket,generate_blog):
+    s3 =boto3.client('s3')
 
-def getLLamaresponse(input_text, no_words, blog_style, creativity):
-    # calling llama 3 -8b model with groq
-    llm = ChatGroq(temperature=creativity,model="llama3-8b-8192",groq_api_key=groq_api_key)
+    try:
+        s3.put_object(Bucket = s3_bucket, Key = s3_key, Body=generate_blog)
+        print("Code saved to s3")
+    except Exception as e:
+        print(f"Error while saving the code to s3 {e}")
+    
+def lambda_handler(event,context):
+    event = json.loads(event['body'])
+    blogtopic = event['blogtopic']
+    generate_blog = blog_gen_bedrock(blogtopic=blogtopic)
 
-    # llm = CTransformers(model ="models\llama-2-7b-chat.ggmlv3.q8_0.bin",
-    #                     model_type = 'llama',
-    #                     config ={'max_new_tokens' :256,
-    #                              'temperature' : 0.01})
-    # template = """
-    # Write a blog for {blog_style} profile for a topic {input_text}
-    # within {no_words} words.
-    # """
-    # prompt = PromptTemplate(input_variables=['blog_style','input_text','no_words'],
-    #                         template=template)
-    prompt = ChatPromptTemplate.from_template("""
-    Write a blog as {blog_style} on a topic {input_text}
-    within {no_words} words.
-    """)
-    # generate the response from llama 2 model
-    chain = prompt | llm
-    response = chain.invoke({"input_text":input_text, "no_words":no_words, "blog_style":blog_style })
-    # response = llm(prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words))
-    print(response.content)
-    return response
-
-
-st.set_page_config(page_title='Generate your choice of Blogs',
-                   layout = 'centered',
-                   initial_sidebar_state='collapsed')
-
-st.header("Generate your choice of Blogs")
-
-input_text = st.text_input("Enter the Blog Topic")
-col1, col2, col3 = st.columns([3,3,3])
-with col1:
-    no_words = st.text_input("No of Words")
-with col2:
-    blog_style = st.selectbox('Writing the blog for',
-                              ('Researcher','Data Scientists','General people','Ocean Vuong','Hank Moody','Chuck Palahniuk'),index=0)
-with col3:
-    creativity = st.text_input("Level of creativity [scale : 0-1]")
-
-submit = st.button('Generate')
-
-# Final Response
-
-if submit:
-    response = getLLamaresponse(input_text, no_words, blog_style, creativity)
-    st.write(response.content)
-
-
-
+    if generate_blog:
+        current_time = datetime.now().strftime("%H%M%S")
+        s3_key = f"blog-output/{current_time}.txt"
+        s3_bucket = "bedrockblog"
+        save_blog_s3(s3_key,s3_bucket,generate_blog)
+    else:
+        print("No blog was generated")
+    
+    return {
+        'statusCode' : 200,
+        'body' : json.dumps('Blog Generated!')
+    }
---
+++ app_str.py
@@ -0,0 +1,66 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain_groq import ChatGroq 
+from langchain_core.prompts import ChatPromptTemplate
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+## Load the Groq API key
+groq_api_key = os.getenv('GROQ_API_KEY')
+# Funciton to get respone from LLAma 2 model
+
+def getLLamaresponse(input_text, no_words, blog_style, creativity):
+    # calling llama 3 -8b model with groq
+    llm = ChatGroq(temperature=creativity,model="llama3-8b-8192",groq_api_key=groq_api_key)
+
+    # llm = CTransformers(model ="models\llama-2-7b-chat.ggmlv3.q8_0.bin",
+    #                     model_type = 'llama',
+    #                     config ={'max_new_tokens' :256,
+    #                              'temperature' : 0.01})
+    # template = """
+    # Write a blog for {blog_style} profile for a topic {input_text}
+    # within {no_words} words.
+    # """
+    # prompt = PromptTemplate(input_variables=['blog_style','input_text','no_words'],
+    #                         template=template)
+    prompt = ChatPromptTemplate.from_template("""
+    Write a blog as {blog_style} on a topic {input_text}
+    within {no_words} words.
+    """)
+    # generate the response from llama 2 model
+    chain = prompt | llm
+    response = chain.invoke({"input_text":input_text, "no_words":no_words, "blog_style":blog_style })
+    # response = llm(prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words))
+    print(response.content)
+    return response
+
+
+st.set_page_config(page_title='Generate your choice of Blogs',
+                   layout = 'centered',
+                   initial_sidebar_state='collapsed')
+
+st.header("Generate your choice of Blogs")
+
+input_text = st.text_input("Enter the Blog Topic")
+col1, col2, col3 = st.columns([3,3,3])
+with col1:
+    no_words = st.text_input("No of Words")
+with col2:
+    blog_style = st.selectbox('Writing the blog for',
+                              ('Researcher','Data Scientists','General people','Ocean Vuong','Hank Moody','Chuck Palahniuk'),index=0)
+with col3:
+    creativity = st.text_input("Level of creativity [scale : 0-1]")
+
+submit = st.button('Generate')
+
+# Final Response
+
+if submit:
+    response = getLLamaresponse(input_text, no_words, blog_style, creativity)
+    st.write(response.content)
+
+
+
